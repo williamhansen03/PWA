9de6a613178adf8d77561ee3ee555788bca09837
williamhansen03 <william.hansen03@skola.stenungsund.se>
2022-10-14 18:03:49 +0200

    Ändrar på serviceworker script url
    Ändrar på serviceworker script ulr

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ if("serviceWorker" in navigator){
     navigator.serviceWorker.register("js/sw.js").then(registration => {
         console.log("SW Registered");
         console.log(registration);
-        navigator.serviceWorker.register("https://williamhansen03.github.io/pwa");
+        ServiceWorkerContainer.register("https://williamhansen03.github.io");
     }).catch(error => {
         console.log("SW Registration Failed!");
         console.log(error);
