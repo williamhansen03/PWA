f52f8e49016e649f2cf367bb2065c9cf6a60a9bd
williamhansen03 <william.hansen03@skola.stenungsund.se>
2022-10-14 17:28:55 +0200

    Flyttar serviuceworker url
    Flyttar serviuceworker url från sw.js till index.js

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ if("serviceWorker" in navigator){
     navigator.serviceWorker.register("js/sw.js").then(registration => {
         console.log("SW Registered");
         console.log(registration);
+        navigator.serviceWorker.register("https://williamhansen03.github.io/pwa");
     }).catch(error => {
         console.log("SW Registration Failed!");
         console.log(error);
